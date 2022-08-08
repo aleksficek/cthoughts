786937c4e302cd06ab95419921eda42627fa109c
Aleksander Ficek <aleksanderficek@Aleksanders-MacBook-Pro.local>
2022-08-08 19:43:57 -0400

    Add initial parsing of subreddit

--- README.md
+++ README.md
@@ -13,3 +13,5 @@
 http://www.cs.cornell.edu/home/llee/omsa/omsa.pdf
 
 https://www.learndatasci.com/tutorials/sentiment-analysis-reddit-headlines-pythons-nltk/
+
+https://www.osrsbox.com/blog/2019/03/18/watercooler-scraping-an-entire-subreddit-2007scape/
--- main.py
+++ main.py
@@ -1,35 +1,49 @@
-from IPython import display
-import math
-from pprint import pprint
-import pandas as pd
-import numpy as np
-import nltk
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set(style='darkgrid', context='talk', palette='Dark2')
+import json
+import re
+from urllib import response
 
-import praw
+symptoms = {}
+remedies = {
+    'niacin': 0,
+    'nac': 0,
+    'ginko': 0,
+    'quercetin': 0,
+    'nattokinase': 0,
+    'natto': 0,
+    'zinc': 0,
+    'probiotics':0,
+    'prebiotics':0,
+    'antihistamines':0,
+}
 
-reddit = praw.Reddit(client_id='44iWCFZcG-EY3287Dcb57A',
-                     client_secret='EEXITDZXLAvhtXBZK6KRyIVubyDqYA',
-                     user_agent='aficek')
+def parse_text(input):
+    parsed_text = re.findall(r"[\w']+|[.,!?;]", input.lower())
+    for each in parsed_text:
+        if each in remedies:
+            remedies[each] += 1
 
-headlines = set()
+def main():
 
-for submission in reddit.subreddit('covidlonghaulers').new(limit=None):
-    headlines.add(submission.title)
-    display.clear_output()
-    print(len(headlines))
+    f = open('data/submissions.json')
+    response_json = json.load(f)
+
+    response_json[0]['title']
+    response_json[0]['selftext']
+
+    for i in range(len(response_json)):
+        print(i)
+
+        parse_text(response_json[i]['title'])
+
+        if "selftext" in response_json[i]:
+            parse_text(response_json[i]['selftext'])
+
+        
+    print("Analysed submissions: ", len(response_json))
+    print("Remedy counts: ", remedies)
 
 
-from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
 
-sia = SIA()
-results = []
 
-for line in headlines:
-    pol_score = sia.polarity_scores(line)
-    pol_score['headline'] = line
-    results.append(pol_score)
-
-pprint(results[:3], width=100)
+if __name__ == "__main__":
+    main()
